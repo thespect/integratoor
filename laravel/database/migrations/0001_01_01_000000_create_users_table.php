f59427e6ff1925eb6a83b6de05fd8c9d91f5b6cb
joseamont <3522110879@uth.edu.mx>
2025-11-12 16:36:38 -0600

    Roles(Creacion de migracion de migracion roles junto con modelo)

--- laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ laravel/database/migrations/2025_11_12_212609_create_roles_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->integer('status')->default(1);
+            $table->dateTime('fecha_creacion');
+            $table->timestamps();
+        });
+
+        DB::table('roles')->insert([
+            [
+                'nombre' => 'admin',
+                'status' => 1,
+                'fecha_creacion' => now(),
+            ],
+            [
+                'nombre' => 'Director de carrera',
+                'status' => 1,
+                'fecha_creacion' => now(),
+            ],
+            [
+                'nombre' => 'Revisor',
+                'status' => 1,
+                'fecha_creacion' => now(),
+            ],
+            [
+                'nombre' => 'Maestr@',
+                'status' => 1,
+                'fecha_creacion' => now(),
+            ]
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rols');
+    }
+};
