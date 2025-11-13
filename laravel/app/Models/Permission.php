2e09f378858f42cca43d909054338687d1ec7dac
joseamont <3522110879@uth.edu.mx>
2025-11-13 15:46:24 -0600

    Modelos(Generacion del modelo de permiso junto con la migraciones de tablas pivotes)

---
+++ laravel/app/Models/Permission.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    //
+    protected $table = 'permission';
+
+    protected $fillable = [
+        'fecha_creacion',
+        'status',
+    ];
+}
